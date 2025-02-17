2d6b489544ffe67efeeeebc087a3ce7777a9ea5a
goodkatt <dh@DH-MacBook-Pro.home>
2025-02-17 23:25:57 +0300

    Initial

--- AWS-Python/get-dynamodb.py
+++ AWS-Python/get-dynamodb.py
@@ -5,7 +5,7 @@ from botocore.exceptions import ClientError
 dynamodb = boto3.resource('dynamodb', 
                           aws_access_key_id=os.getenv('AWS_ACCESS_KEY_ID'),
                           aws_secret_access_key=os.getenv('AWS_SECRET_ACCESS_KEY'),
-                          region_name=os.getenv('AWS_REGION'))  
+                          region_name='eu-west-1')  
 table = dynamodb.Table('adservice_versions')  
 
 def get_version(version_key):
